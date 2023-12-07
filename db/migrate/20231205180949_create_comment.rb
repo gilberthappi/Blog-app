1a14539b44acde4218b023177b11670d08bd132c
gilberthappi <gdushimimana6@gmail.com>
2023-12-07 22:57:11 +0200

    Updating linter checker

---
+++ db/migrate/20231205180949_create_comment.rb
@@ -0,0 +1,10 @@
+class CreateComment < ActiveRecord::Migration[7.1]
+  def change
+    create_table :comments do |t|
+      t.references   :user, null: false, foreign_key: { to_table: 'users' }, index: true
+      t.references   :post, null: false, foreign_key: { to_table: 'posts' }, index: true
+      t.text      :text 
+      t.timestamps
+    end
+  end
+end
